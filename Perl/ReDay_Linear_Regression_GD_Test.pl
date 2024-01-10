26dd1fc57896bddd3ace7917120a9d2a990b6c69
ReDay Zarra <redayzarra@gmail.com>
2024-01-09 20:14:23 -0500

    Calculate the mean squared error from linear regression

--- Perl/ReDay_Linear_Regression_GD_Test.pl
+++ Perl/ReDay_Linear_Regression_GD_Test.pl
@@ -1,7 +1,24 @@
 # Include the main script
-require 'Linear_Regression_GD.pl';
+require './ReDay_Linear_Regression_GD.pl';
 
-# Define sample data (x, y)
-my @x_values = (1, 2, 3, 4, 5);
-my @y_values = (2, 3, 4, 5, 6);
+# Ensure the number of data points is defined
+my $n = scalar @x_values;
 
+# Calculated Theta values
+my $theta_0 = 0.876332021468342;
+my $theta_1 = 1.03425404703086;
+
+my $sum_of_squared_errors = 0;
+for (my $i = 0; $i < scalar @x_values; $i++) {
+    my $predicted = $theta_0 + $theta_1 * $x_values[$i];
+    my $observed = $y_values[$i];
+    $sum_of_squared_errors += ($predicted - $observed) ** 2;
+}
+# Calculate the Mean Squared Error
+my $mse = $sum_of_squared_errors / $n;
+
+# Round the Mean Squared Error to 2 decimal places
+my $rounded_mse = sprintf("%.5f", $mse);
+
+# Print the Mean Squared Error
+print "Mean Squared Error: $rounded_mse\n";
