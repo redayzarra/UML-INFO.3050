76aa523f2161e2bd52e3a6fcb68d58f0e523e48e
ReDay Zarra <redayzarra@gmail.com>
2024-01-09 19:07:57 -0500

    Choosing to reimplement gradient descent for linear regression

---
+++ Perl/ReDay_Linear_Regression_GD.pl
@@ -0,0 +1,13 @@
+# Make sure Perl has been added to system path at Strawberry/perl/bin
+
+"""
+Assignment 2 - Complex Perl Program 
+
+Instructions: I want you to choose something not too simple, but not something too hard for you
+either. When figuring out what 'moderate complex' means to you, remember I am only
+giving you a short time to write this code (2 weeks), so err on the side of easier rather
+than harder. That will be OK. Remember, I prefer higher quality code, complete work,
+than a more ambitious, but incomplete or rushed work. 
+
+Requirements: I prefer a high quality to high quantity.
+"""
