a8951db6d9219bb80966f1072fecd3862eb27452
ReDay Zarra <redayzarra@gmail.com>
2024-01-09 19:16:28 -0500

    Setting up variables for gradient descent

--- Perl/ReDay_Linear_Regression_GD.pl
+++ Perl/ReDay_Linear_Regression_GD.pl
@@ -1,7 +1,9 @@
 # Make sure Perl has been added to system path at Strawberry/perl/bin
 
+use strict;
+use warnings;
 """
-Assignment 2 - Complex Perl Program 
+Assignment 2 - Complex Perl Program: Linear Regression Gradient Descent
 
 Instructions: I want you to choose something not too simple, but not something too hard for you
 either. When figuring out what 'moderate complex' means to you, remember I am only
@@ -11,3 +13,18 @@ than a more ambitious, but incomplete or rushed work.
 
 Requirements: I prefer a high quality to high quantity.
 """
+
+# Theta parameters for bias and slope coefficients
+my $theta_0 = 0; # Intercept
+my $theta_1 = 0; # Slope
+
+# The learning rate - decides how big of a step we take in any direction
+my $alpha = 0.01;
+
+# Number of iterations which need to played around with - ensemble method
+# in machine learning is when another model would manipulate these variables.
+my $iterations = 1000;
+
+# Number of training examples
+my $n = scalar @x_values;
+
