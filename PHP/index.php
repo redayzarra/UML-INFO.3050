53dbe47304dfbb4f34a5d3613fa46a0eb8de6454
ReDay Zarra <redayzarra@gmail.com>
2024-01-12 17:34:18 -0500

    Removed SimpleWebApp.zip and updated repository

---
+++ PHP/index.php
@@ -0,0 +1,1 @@
+echo 'Is this working?';
---
+++ PHP/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '292cd5625097bf7a9d58a9b34c5187c76301451b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '292cd5625097bf7a9d58a9b34c5187c76301451b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'cocur/slugify' => array(
+            'pretty_version' => 'v4.5.1',
+            'version' => '4.5.1.0',
+            'reference' => '7c6e088228b9f082050876ae8b0cd287b117b840',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../cocur/slugify',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
