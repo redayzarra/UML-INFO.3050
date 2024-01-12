423aa4baeeb7148abaf47dffd1074a1bb1007718
ReDay Zarra <redayzarra@gmail.com>
2024-01-11 20:43:40 -0500

    Simple web app backend server is good to go

---
+++ Python/SimpleWebApp/backend/server/app.py
@@ -0,0 +1,33 @@
+import os
+from flask import Flask
+from flask import Response, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config.from_mapping(
+    DEBUG=False,
+    WEB_ADDRESS="0.0.0.0",
+    WEB_PORT=8080,
+)
+
+# Enable CORS
+CORS(app, resources={r"/drone/*": {"origins": "*"}})
+
+# The logging has already been set up
+app.logger.info("Server started.")
+
+@app.route("/drone/takeoff", methods=["POST"])
+def takeoff():
+    try:
+        app.logger.info(f"App is up and running.")
+        return jsonify({"status": "success", "response": "We are good to go."}), 200
+    except Exception as e:
+        app.logger.error(f"Error during start up.")
+        return jsonify({"status": "error", "message": "God damn flask."}), 500
+
+
+if __name__ == "__main__":
+    try:
+        app.run(debug = False, host="0.0.0.0", port=8080)
+    except Exception as e:
+        app.logger.error(f"An error occurred: {str(e)}")
